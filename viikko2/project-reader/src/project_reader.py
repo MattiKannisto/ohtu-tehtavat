acbdae3c602635540e5d3dcad8465167448cefcd
MattiKannisto <matti.kannisto@helsinki.fi>
2021-03-28 18:47:45 +0300

    Lisää viikon 2 tehtävät 1 ja 2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,15 @@
+from urllib import request
+from project import Project
+import toml
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        parsed_toml = toml.loads(content)
+        return Project(parsed_toml["tool"]["poetry"]["name"], parsed_toml["tool"]["poetry"]["description"], parsed_toml["tool"]["poetry"]["dependencies"], parsed_toml["tool"]["poetry"]["dev-dependencies"])
